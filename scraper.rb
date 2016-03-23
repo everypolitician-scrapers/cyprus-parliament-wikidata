e4ddc7eb4ab6145c2fcd64124bc94a22df99b442
Tony Bowden <tony@tmtm.com>
2016-03-23 17:21:11 +0000

    Source now contains Wikidata IDs

--- scraper.rb
+++ scraper.rb
@@ -1,20 +1,7 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require 'pry'
 require 'wikidata/fetcher'
 
-links = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/cyprus-openpatata', column: 'wikipedia')
-
-def urls_to_names(a)
-  a.map { |u| URI.decode File.basename u }
-end
-
-by_site = links.group_by { |p| p[/\/\/(\w+).wikipedia.org/, 1] }
-EveryPolitician::Wikidata.scrape_wikidata(names: { 
-  en: urls_to_names(by_site['en']),
-  el: urls_to_names(by_site['el']),
-})
-
-warn EveryPolitician::Wikidata.notify_rebuilder
-
+ids = EveryPolitician::Wikidata.morph_wikinames(source: 'tmtmtmtm/cyprus-openpatata', column: 'identifier__wikidata')
+EveryPolitician::Wikidata.scrape_wikidata(ids: ids)
